97d26a5d3ccf0d3e241a9fd191250fe5dbfdaf36
dsddr02 <fldz5555a@outlook.com>
2025-08-15 12:41:43 +0800

    Update cloudflarespeedtest.sh

--- applications/luci-app-cloudflarespeedtest/root/usr/bin/cloudflarespeedtest/cloudflarespeedtest.sh
+++ applications/luci-app-cloudflarespeedtest/root/usr/bin/cloudflarespeedtest/cloudflarespeedtest.sh
@@ -4,7 +4,7 @@ LOG_FILE='/var/log/cloudflarespeedtest.log'
 IP_FILE='/usr/share/cloudflarespeedtestresult.txt'
 IPV4_TXT='/usr/share/CloudflareSpeedTest/ip.txt'
 IPV6_TXT='/usr/share/CloudflareSpeedTest/ipv6.txt'
-IP_URL='https://dz.pk67.dpdns.org/ivv.txt?token=zf004'
+IP_URL='https://dz.pk7.dpdns.org/ivv.txt?token=zf004'
 
 function get_global_config(){
     while [[ "$*" != "" ]]; do
@@ -81,21 +81,7 @@ send_telegram_message() {
 function speed_test(){
     rm -rf $LOG_FILE
 
-    if [ $ipv6_enabled -eq "1" ]; then
-        echolog "开始下载IPv6 IP列表..."
-        if ! download_ip_file "$IP_URL" "$IPV6_TXT"; then
-            echolog "错误: 无法获取IPv6 IP列表，测速终止"
-            return 1
-        fi
-    else
-        echolog "开始下载IPv4 IP列表..."
-        if ! download_ip_file "$IP_URL" "$IPV4_TXT"; then
-            echolog "错误: 无法获取IPv4 IP列表，测速终止"
-            send_telegram_message "错误: 无法获取IPv4 IP列表，测速终止"
-            return 1
-        fi
-    fi
-
+    
     command="/usr/bin/cdnspeedtest -sl $((speed*125/1000)) -url ${custome_url} -o ${IP_FILE}"
 
     if [ $ipv6_enabled -eq "1" ] ;then
@@ -138,22 +124,51 @@ function speed_test(){
 }
 
 function ip_replace(){
-    bestip=$(sed -n "2,1p" $IP_FILE | awk -F, '{print $1}')
-    if [[ -z "${bestip}" ]]; then
-        echolog "CloudflareST 测速结果 IP 数量为 0,跳过下面步骤..."
-    else
-        passwall2_best_ip
-        restart_app
+    # Read all IPs from the IP_FILE
+    ips=$(awk -F, 'NR>1{print $1}' "$IP_FILE")
+    
+    # Count the number of IPs
+    ip_count=$(echo "$ips" | wc -l)
+    
+    # Count the number of ssrnames
+    ssrname_count=$(echo "$passwall2_services" | wc -w)
+
+    if [ -z "$ips" ]; then
+        echolog "CloudflareST 测速结果 IP 数量为 0, 跳过下面步骤..."
+        return
     fi
+
+    if [ "$ip_count" -gt "$ssrname_count" ]; then
+        echolog "警告: IP 数量 ($ip_count) 大于 passwall2 服务数量 ($ssrname_count).  只会使用前 $ssrname_count 个 IP."
+        # Truncate the ips variable to only contain the first ssrname_count IPs
+        ips=$(echo "$ips" | head -n "$ssrname_count")
+    fi
+
+    passwall2_best_ip "$ips"
+    restart_app
 }
 
 function passwall2_best_ip(){
+    local ips="$1"
+    local i=0
+
     if [ "x${passwall2_enabled}" == "x1" ] ;then
-        echolog "设置passwall2 IP"
+        echolog "设置 passwall2 IP"
+        
+        # Iterate through ssrnames and IPs
         for ssrname in $passwall2_services
         do
-            echo $ssrname
-            uci set passwall2.$ssrname.address="${bestip}"
+            # Get the i-th IP from the list of IPs
+            ip=$(echo "$ips" | awk "NR==$(($i+1))")
+            
+            if [ -n "$ip" ]; then
+                echolog "设置 $ssrname 的 IP 为: $ip"
+                uci set passwall2."$ssrname".address="$ip"
+            else
+                echolog "没有足够的 IP 为 $ssrname 设置 IP. 跳过."
+            fi
+            
+            i=$((i+1))
         done
         uci commit passwall2
     fi
@@ -163,14 +178,16 @@ function restart_app(){
     if [ "x${passwall2_started}" == "x1" ] ;then
         if [ $proxy_mode  == "close" ] ;then
             uci set passwall2.@global[0].enabled="${passwall2_server_enabled}"
-            elif [ $proxy_mode  == "gfw" ] ;then
+        elif [ $proxy_mode  == "gfw" ] ;then
             uci set passwall2.@global[0].tcp_proxy_mode="${passwall2_original_run_mode}"
         fi
         uci commit passwall2
         /etc/init.d/passwall2 restart 2>/dev/null
         echolog "passwall2重启完成"
         sleep 4
-        send_telegram_message "设置passwall2IP${bestip}"
+        # Send Telegram message with the new IP(s).  Join with commas for brevity
+        new_ips=$(uci show passwall2 | grep address= | awk -F'[=]' '{print $2}' | tr '\n' ',')
+        send_telegram_message "设置passwall2IP" 
     fi
 }
 
